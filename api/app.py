c0decaa2417284ab00886a2ed6465ca9060c6974
Yohan <yshanuka123@gmail.com>
2026-01-10 13:56:04 +0530

    API endpoint is created

---
+++ api/app.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import os
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "..",
+    "saved_models",
+    "1.keras"
+)
+
+print("MODEL PATH:", MODEL_PATH)
+print("EXISTS:", os.path.exists(MODEL_PATH))
+
+
+MODEL = tf.keras.models.load_model(MODEL_PATH)
+
+CLASS_NAMES = [ 'Tomato_Bacterial_spot',
+                'Tomato_Early_blight',
+                'Tomato_Late_blight',
+                'Tomato_Leaf_Mold',
+                'Tomato__Target_Spot',
+                'Tomato_healthy' ]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, World!"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())  # Read the uploaded file (you can process it as needed)
+    img_batch = np.expand_dims(image, 0)  # Expand dimensions to match model input shape
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
